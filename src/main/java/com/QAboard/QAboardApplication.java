2502bdd877ea182cc5640a041f4451bda6779048
Meng1024 <meng@Mengs-MBP.lan>
2016-11-06 00:32:28 -0700

    Start a new web application

---
+++ src/main/java/com/QAboard/QAboardApplication.java
@@ -0,0 +1,12 @@
+package com.QAboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QAboardApplication.class, args);
+	}
+}
