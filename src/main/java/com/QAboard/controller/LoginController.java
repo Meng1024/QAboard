07ffbc13076607bdc53fc6cebde10fd5c96e062d
Meng1024 <mengmeng2811@gmail.com>
2016-11-29 01:29:40 -0800

    Sign in and Sign up

---
+++ src/main/java/com/QAboard/controller/LoginController.java
@@ -0,0 +1,83 @@
+package com.QAboard.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.QAboard.service.UserService;
+
+
+@Controller
+public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    @Autowired
+    UserService userService;
+    @RequestMapping(path = {"/reg/"}, method = {RequestMethod.POST})
+    public String reg(Model model, @RequestParam("username") String username,
+                      @RequestParam("password") String password,
+                      @RequestParam(value="rememberme", defaultValue = "false") boolean rememberme,
+                      HttpServletResponse response) {
+        try {
+            Map<String, String> map = userService.register(username, password);
+            if (map.containsKey("ticket")) {
+                Cookie cookie = new Cookie("ticket", map.get("ticket").toString());
+                cookie.setPath("/");
+                if (rememberme) {
+                    cookie.setMaxAge(3600*24*5);
+                }
+                response.addCookie(cookie);
+                return "redirect:/";
+            } else {
+                model.addAttribute("msg", map.get("msg"));
+                return "login";
+            }
+
+        } catch (Exception e) {
+            logger.error("注册异常" + e.getMessage());
+            model.addAttribute("msg", "服务器错误");
+            return "login";
+        }
+    }
+
+    @RequestMapping(path = {"/reglogin"}, method = {RequestMethod.GET})
+    public String regloginPage() {
+        return "login";
+    }
+
+    @RequestMapping(path = {"/login/"}, method = {RequestMethod.POST})
+    public String login(Model model, @RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        @RequestParam(value="rememberme", defaultValue = "false") boolean rememberme,
+                        HttpServletResponse response) {
+        try {
+            Map<String, String> map = userService.login(username, password);
+            if (map.containsKey("ticket")) {
+                Cookie cookie = new Cookie("ticket", map.get("ticket").toString());
+                cookie.setPath("/");
+                if (rememberme) {
+                    cookie.setMaxAge(3600*24*5);
+                }
+                response.addCookie(cookie);
+                return "redirect:/index";
+            } else {
+                model.addAttribute("msg", map.get("msg")+ "hi there");
+                return "login";
+            }
+
+        } catch (Exception e) {
+            logger.error("登陆异常" + e.getMessage());
+            return "login";
+        }
+    }
+
+}
--- src/main/java/com/QAboard/service/UserService.java
+++ src/main/java/com/QAboard/service/UserService.java
@@ -1,11 +1,21 @@
 package com.QAboard.service;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import com.QAboard.dao.LoginTicketDAO;
 import com.QAboard.dao.UserDAO;
+import com.QAboard.model.LoginTicket;
 import com.QAboard.model.User;
+import com.QAboard.util.QAboardUtil;
 
 @Service
 public class UserService {
@@ -13,6 +23,74 @@ public class UserService {
     @Autowired
     private UserDAO userDAO;
     
+    @Autowired
+    private LoginTicketDAO loginTicketDAO;
+    
+    public Map<String, String> register(String username, String password) {
+        Map<String, String> map = new HashMap<>();
+        if(StringUtils.isEmpty(username)) {
+            map.put("msg", "The username cannot be empty");
+            return map;
+        }
+        if(StringUtils.isEmpty(password)) {
+            map.put("msg", "The password cannot be empty");
+            return map;
+        }
+        User user = userDAO.selectByName(username);
+        if(user != null) {
+            map.put("msg", "The username already existed, please try another one");
+            return map;
+        }
+        user = new User();
+        user.setName(username);
+        user.setSalt(UUID.randomUUID().toString().substring(0, 5));
+        user.setHeadUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        user.setPassword(QAboardUtil.MD5(password + user.getSalt())); 
+        userDAO.addUser(user); 
+        
+        String ticket = addLoginTicket(user.getId());
+        map.put("ticket", ticket);
+        return map;
+    }
+    public Map<String, String> login(String username, String password) {
+        Map<String, String> map = new HashMap<>();
+        if(StringUtils.isEmpty(username)) {
+            map.put("msg", "The username cannot be empty");
+            return map;
+        }
+        
+        if(StringUtils.isEmpty(password)) {
+            map.put("msg", "The password cannot be empty");
+            return map;
+        }
+        
+        User user = userDAO.selectByName(username);
+        if(user == null) {
+            map.put("msg", "Username does not exist");
+            return map;
+        }
+        
+        if(!QAboardUtil.MD5(password + user.getSalt()).equals(user.getPassword())) {
+            map.put("msg", "Wrong password or username");
+            return map;
+        }
+        String ticket = addLoginTicket(user.getId());
+        map.put("ticket", ticket);
+        return map;
+    }
+    
+    public String addLoginTicket(int userId) {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(userId);
+        Date now = new Date();
+        now.setTime(now.getTime() + 3600*24*100);
+        loginTicket.setExpired(now);
+        loginTicket.setStatus(0);
+        loginTicket.setTicket(UUID.randomUUID().toString().replaceAll("-", ""));
+        loginTicketDAO.addTicket(loginTicket);
+        return loginTicket.getTicket();
+
+    }
     public User getUser(int id) {
         return userDAO.selectById(id);
     }
